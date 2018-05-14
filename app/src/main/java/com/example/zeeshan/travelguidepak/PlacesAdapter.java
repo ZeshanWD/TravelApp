ee7e146b2d3d5bddcebebe5b599bffad859247ea
Zeshan choudhary <zeeshan9choudhary@gmail.com>
2018-05-14 22:26:53 +0200

    Added user model n tried fetching user but not working yet

--- app/src/main/java/com/example/zeeshan/travelguidepak/PlacesAdapter.java
+++ app/src/main/java/com/example/zeeshan/travelguidepak/PlacesAdapter.java
@@ -1,18 +1,40 @@
 package com.example.zeeshan.travelguidepak;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.w3c.dom.Text;
+
 import java.util.List;
+
+import static android.support.constraint.Constraints.TAG;
 
 public class PlacesAdapter extends RecyclerView.Adapter<PlacesAdapter.ViewHolder> {
 
     private List<Place> lista;
+    private Context context;
+    private FirebaseFirestore firebaseFirestore;
+    private User user;
     public PlacesAdapter(List<Place> listaSitios){
         this.lista = listaSitios;
     }
@@ -20,6 +42,7 @@ public class PlacesAdapter extends RecyclerView.Adapter<PlacesAdapter.ViewHolder
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.place_list_item, parent, false);
+        context = parent.getContext();
         return new ViewHolder(view);
     }
 
@@ -29,6 +52,24 @@ public class PlacesAdapter extends RecyclerView.Adapter<PlacesAdapter.ViewHolder
         String descData = lista.get(position).getDescription();
         holder.setDesc(descData);
 
+        String imageUrl = lista.get(position).getImage();
+        holder.setImage(imageUrl);
+
+        String userId = lista.get(position).getUserId();
+        User username = getUser(userId);
+        holder.setUsername(username.getName());
+
+
+    }
+
+    private User getUser(String userId){
+        firebaseFirestore.collection("Users").document(userId).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+               user = documentSnapshot.toObject(User.class);
+            }
+        });
+        return user;
     }
 
     @Override
@@ -40,6 +81,8 @@ public class PlacesAdapter extends RecyclerView.Adapter<PlacesAdapter.ViewHolder
 
         private View mView;
         private TextView descView;
+        private ImageView placeImage;
+        private TextView username;
 
 
         public ViewHolder(View itemView) {
@@ -50,6 +93,16 @@ public class PlacesAdapter extends RecyclerView.Adapter<PlacesAdapter.ViewHolder
         private void setDesc(String desc){
             descView = mView.findViewById(R.id.place_desc);
             descView.setText(desc);
+        }
+
+        private void setImage(String imageUrl){
+            placeImage = mView.findViewById(R.id.place_image);
+            Glide.with(context).load(imageUrl).into(placeImage);
+        }
+
+        private void setUsername(String name){
+            username = mView.findViewById(R.id.place_username);
+            username.setText(name);
         }
     }
 }
---
+++ app/src/main/java/com/example/zeeshan/travelguidepak/User.java
@@ -0,0 +1,29 @@
+package com.example.zeeshan.travelguidepak;
+
+public class User {
+    private String name, imageUrl;
+
+    public User(){
+
+    }
+    public User(String name, String imageUrl) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
